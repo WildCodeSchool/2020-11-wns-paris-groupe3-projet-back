1d4cc68fec11267abe5d834593b6dfdfd006d11e
Jérémy <jeremy.vaneste@free.fr>
2021-01-07 11:39:05 +0100

    server.integration -> just test apollo server

--- src/server.integration.spec.ts
+++ src/server.integration.spec.ts
@@ -1,1 +1,26 @@
-console.log("Fichier Test Vide");
+import { createTestClient } from "apollo-server-testing";
+import {
+  connect,
+  closeDatabase,
+  clearDatabase,
+} from "./mongo/config_db_testing";
+
+import { getApolloServer } from "./server";
+import { Task } from "./models/task";
+import { User } from "./models/user";
+
+describe("Apollo server", () => {
+  let mutate: any;
+  let query: any;
+  beforeEach(async () => {
+    await connect();
+    const testClient = createTestClient(await getApolloServer());
+    mutate = testClient.mutate;
+    query = testClient.query;
+  });
+
+  afterEach(async () => {
+    await clearDatabase();
+    await closeDatabase();
+  });
+});
