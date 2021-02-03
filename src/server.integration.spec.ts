3e864601947a3c2753c9ab7f33060984e19de168
Lola <lola.donval@gmail.com>
2021-02-03 18:23:35 +0100

    removed useless import and variable

--- src/server.integration.spec.ts
+++ src/server.integration.spec.ts
@@ -6,16 +6,13 @@ import {
 } from "./mongo/config_db_testing";
 
 import { getApolloServer } from "./server";
-import { Task } from "./models/task";
 import { User } from "./models/user";
 
 describe("Apollo server", () => {
-  let mutate: any;
   let query: any;
   beforeEach(async () => {
     await connect();
     const testClient = createTestClient(await getApolloServer());
-    mutate = testClient.mutate;
     query = testClient.query;
   });
 
