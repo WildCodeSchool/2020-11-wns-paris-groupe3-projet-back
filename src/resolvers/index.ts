48eaffc30ed03c22cd2141e67683ed5653fbb0de
Lola <lola.donval@gmail.com>
2021-02-04 16:13:24 +0100

    added new resolvers into index.js

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,7 +2,14 @@ import { merge } from "lodash";
 
 import { userResolvers } from "./userResolvers";
 import { taskResolvers } from "./taskResolvers";
+import { renderResolvers } from "./renderResolvers";
+import { correctionResolvers } from "./correctionResolvers";
 
-const resolvers = merge(userResolvers, taskResolvers);
+const resolvers = merge(
+  userResolvers,
+  taskResolvers,
+  renderResolvers,
+  correctionResolvers
+);
 
 export default resolvers;
