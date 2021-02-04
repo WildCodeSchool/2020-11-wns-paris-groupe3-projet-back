3ff1fe42c0318b234ba639d6ada90f2347253736
Lola <lola.donval@gmail.com>
2021-02-04 13:23:43 +0100

    created utils folder and put on it cloudinaryConfig function. added try catch to procede cloudinary request before creationof a new task

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,20 +1,14 @@
 import mongodb from "mongodb";
-import cloudinary from "cloudinary";
 import { Document } from "mongoose";
+import cloudinary from "cloudinary";
+
+import { cloudinaryConfig } from "../utils/cloudinary";
 
 import { Task } from "../models/task";
 import { User } from "../models/user";
 import { Render } from "../models/render";
 import { Correction } from "../models/correction";
 import { RenderType, TaskType, CorrectionType } from "allTypes";
-
-const cloudinaryConfig = () => {
-  cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-};
 
 const ObjectID = mongodb.ObjectID;
 
@@ -27,12 +21,14 @@ const resolvers = {
     createTask: async (parent: any, args: any): Promise<Document> => {
       cloudinaryConfig();
       try {
-        const result = await cloudinary.v2.uploader.upload_large(
-          args.input.url,
-          {
+        let result;
+        try {
+          result = await cloudinary.v2.uploader.upload(args.input.url, {
             folder: "EasyHomeworks",
-          }
-        );
+          });
+        } catch (e) {
+          return e.message;
+        }
         const newTask: TaskType = {
           _id: new ObjectID(),
           taskname: args.input.taskname,
@@ -50,10 +46,14 @@ const resolvers = {
     createRender: async (parent: any, args: any): Promise<Document> => {
       cloudinaryConfig();
       try {
-        const result = await cloudinary.v2.uploader.upload(args.input.url, {
-          allowed_formats: ["jpg", "png"],
-          folder: "EasyHomeworks",
-        });
+        let result;
+        try {
+          result = await cloudinary.v2.uploader.upload(args.input.url, {
+            folder: "EasyHomeworks",
+          });
+        } catch (e) {
+          return e.message;
+        }
         const newRender: RenderType = {
           _id: new ObjectID(),
           url: result.secure_url,
@@ -71,10 +71,14 @@ const resolvers = {
     createCorrection: async (parent: any, args: any): Promise<Document> => {
       cloudinaryConfig();
       try {
-        const result = await cloudinary.v2.uploader.upload(args.input.url, {
-          allowed_formats: ["jpg", "png"],
-          folder: "EasyHomeworks",
-        });
+        let result;
+        try {
+          result = await cloudinary.v2.uploader.upload(args.input.url, {
+            folder: "EasyHomeworks",
+          });
+        } catch (e) {
+          return e.message;
+        }
         const newCorrection: CorrectionType = {
           _id: new ObjectID(),
           url: result.secure_url,
