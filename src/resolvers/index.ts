6654bcfed90af491b6c76710d00e45c2746bbb53
Lola <lola.donval@gmail.com>
2021-02-18 12:30:25 +0100

    added query to fetch all classrooms

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -3,7 +3,13 @@ import { merge } from "lodash";
 import { userResolvers } from "./userResolvers";
 import { taskResolvers } from "./taskResolvers";
 import { taskAssignationResolvers } from "./taskAssignationResolvers";
+import { classroomResolvers } from "./classroomResolvers";
 
-const resolvers = merge(userResolvers, taskResolvers, taskAssignationResolvers);
+const resolvers = merge(
+  userResolvers,
+  taskResolvers,
+  taskAssignationResolvers,
+  classroomResolvers
+);
 
 export default resolvers;
